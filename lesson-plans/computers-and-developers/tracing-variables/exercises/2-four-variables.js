4f6f6a5ec5d4ed7c9eb153608d77c44069de4f51
Harley Clair <60947601+harley-dane@users.noreply.github.com>
2025-02-10 21:01:55 +0100

    variable

--- lesson-plans/computers-and-developers/tracing-variables/exercises/2-four-variables.js
+++ lesson-plans/computers-and-developers/tracing-variables/exercises/2-four-variables.js
@@ -11,20 +11,40 @@
 
 // trace the program a few times with different values
 
-let a = 'Pieter';
+// DECLARE a variable named `a`
+// INITIALIZE the value to "Pieter"
+
+let a = ' Pieter';
 // let a = 'Blake';
 // let a = 'George';
+
+// DECLARE a variable named `b`
+// INITIALIZE the value to "Rabbit"
 
 let b = 'Rabbit';
 // let b = 'Wozzeck';
 // let b = 'Schoenberg';
 
+// DECLARE a variable named `c`
+// CONCATENATE  Pieter  + empty string +  Rabbit
+
 let c = a + ' ' + b;
+
+// DECLARE a variable named `d`
+// INITIALIZE the value to hello
+// CONCATENATE  Pieter + " "  + Rabbit
 
 let d = 'hello ' + c;
 
+// READ the value stored in `d`
+
 console.log(d);
 
+// ASSIGN a new value to `d`
+// CONCATENATE 'good bye ' Pieter + Rabbit
+
 d = 'good bye ' + c;
+
+// READ the value stored in `d`
 
 console.log(d);
